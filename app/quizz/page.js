896fa13d446c1bf6c4ee72736e308c635e87d9b2
Samir3200 <samirelorf@yahoo.fr>
2026-05-16 18:03:26 +0200

    in build

---
+++ app/quizz/page.js
@@ -0,0 +1,8 @@
+export default function QuizzPage() {
+  return (
+    <div style={{ minHeight: '80vh' }} className="flex flex-col items-center justify-center text-gray-400 font-mono text-lg">
+      <span role="img" aria-label="construction" style={{ fontSize: 48, marginBottom: 16 }}>🚧</span>
+      <div>Page "Quizz" en construction…</div>
+    </div>
+  );
+}
